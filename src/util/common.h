d0d88bea3a4f2cbf1b7b0579697a252928ae59ee
danfortunato <dan.fortunato@gmail.com>
2017-06-20 18:15:55 -0700

    Template mass matrices on dimension

---
+++ src/util/common.h
@@ -0,0 +1,24 @@
+#ifndef COMMON
+#define COMMON
+
+#define EIGEN_NO_DEBUG
+#define EIGEN_USE_MKL_ALL
+#define MKL_ALIGN 64
+
+#include <Eigen/Dense>   // Eigen
+#include <mkl.h>         // MKL
+#include <mkl_spblas.h>  // MKL routines
+
+namespace DG
+{
+    template<typename T>
+    constexpr T ipow(T x, int n)
+    {
+        return (n >= sizeof(int)*8) ? 0 : n == 0 ? 1 : x * ipow(x, n-1);
+    }
+
+    template<int P, int D>
+    using Mat = Eigen::Matrix<double,ipow(P,D),ipow(P,D),Eigen::RowMajor>;
+}
+
+#endif
