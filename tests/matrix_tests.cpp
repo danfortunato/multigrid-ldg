9b51bd9081378b39d173f38dbd59ca2de3efa246
danfortunato <dan.fortunato@gmail.com>
2017-06-21 15:19:27 -0700

    Rename namespace to DG

--- tests/matrix_tests.cpp
+++ tests/matrix_tests.cpp
@@ -1,27 +1,27 @@
-#include "SparseBlockMatrix.h"
+#include "sparseblockmatrix.h"
 #include <iostream>
 
 int main()
 {
     const unsigned int N = 4;
-    LDG::SparseBlockMatrixBuilder<N> T(2,2);
-    typedef LDG::SparseBlockMatrix<N>::Block Mat;
+    DG::SparseBlockMatrixBuilder<N> T(2,2);
+    typedef DG::SparseBlockMatrix<N>::Block Mat;
     Mat R = Mat::Random();
 
     // Build a matrix
     T.setBlock(0,0,R);
     T.setBlock(1,1,R*R);
-    LDG::SparseBlockMatrix<N> A = T.build();
+    DG::SparseBlockMatrix<N> A = T.build();
 
     // Build a second matrix
     T.reset(2,3);
     T.setBlock(0,0,R);
     T.setBlock(1,0,R);
     T.setBlock(1,2,R*R);
-    LDG::SparseBlockMatrix<N> B = T.build();
+    DG::SparseBlockMatrix<N> B = T.build();
 
-    LDG::SparseBlockMatrix<N> C;
-    bool success = LDG::multiply_mm(A, B, C);
+    DG::SparseBlockMatrix<N> C;
+    bool success = DG::multiply_mm(A, B, C);
     std::cout << success << std::endl;
 
     return 0;
