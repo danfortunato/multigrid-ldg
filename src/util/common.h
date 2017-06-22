9b22fced057a2be5ef2731c9f5c627086ae05c55
danfortunato <dan.fortunato@gmail.com>
2017-06-21 23:12:45 -0700

    Make Coordinate use std::array

--- src/util/common.h
+++ src/util/common.h
@@ -8,6 +8,7 @@
 #include <Eigen/Dense>   // Eigen
 #include <mkl.h>         // MKL
 #include <mkl_spblas.h>  // MKL routines
+#include <array>         // std::array
 
 namespace DG
 {
@@ -27,53 +28,28 @@ namespace DG
     template<int N>
     struct Coordinate
     {
-            /** @brief Constructor from value */
-            Coordinate(double v = 0) { for (int i = 0; i < N; ++i) { x[i] = v; } }
-            /** @brief Access operator */
-            inline double& operator[] (int i) { return x[i]; }
-            inline const double& operator[] (int i) const { return x[i]; }
-            /** @brief Array of coordinate components */
-            double x[N];
-    };
+        /** @brief Constructor from value */
+        Coordinate(double v = 0)
+        {
+            for (int i = 0; i < N; ++i) {
+                x[i] = v;
+            }
+        }
 
-    template<>
-    struct Coordinate<2>
-    {
-        Coordinate(double v = 0) : x(v), y(v) {}
-        Coordinate(double x_, double y_) : x(x_), y(y_) {}
-        inline double& operator[] (int i) {
-            if (i < 1 || i > 2) {
-                throw std::out_of_range("Requested component does not exist.");
+        /** @brief Constructor from values */
+        Coordinate(std::array<double,N> v)
+        {
+            for (int i = 0; i < N; ++i) {
+                x[i] = v[i];
             }
-            return i==1 ? x : y;
         }
-        inline const double& operator[] (int i) const {
-            if (i < 1 || i > 2) {
-                throw std::out_of_range("Requested component does not exist.");
-            }
-            return i==1 ? x : y;
-        }
-        double x, y;
-    };
 
-    template<>
-    struct Coordinate<3>
-    {
-        Coordinate(double v = 0) : x(v), y(v), z(v) {}
-        Coordinate(double x_, double y_, double z_) : x(x_), y(y_), z(z_) {}
-        inline double& operator[] (int i) {
-            if (i < 1 || i > 3) {
-                throw std::out_of_range("Requested component does not exist.");
-            }
-            return i==1 ? x : (i==2 ? y : z);
-        }
-        inline const double& operator[] (int i) const {
-            if (i < 1 || i > 3) {
-                throw std::out_of_range("Requested component does not exist.");
-            }
-            return i==1 ? x : (i==2 ? y : z);
-        }
-        double x, y, z;
+        /** @brief Access operator */
+        inline double& operator[] (int i) { return x[i]; }
+        inline const double& operator[] (int i) const { return x[i]; }
+
+        /** @brief Array of coordinate components */
+        std::array<double,N> x;
     };
 
     /** @brief Equals operator */
