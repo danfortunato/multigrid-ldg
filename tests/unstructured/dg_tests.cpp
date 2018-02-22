85f069a8a2112d5b0a54ed7ba18820f66705e9ef
danfortunato <dan.fortunato@gmail.com>
2018-02-22 14:54:22 -0500

    Normalize quadrature weights to sum to 1

--- tests/unstructured/dg_tests.cpp
+++ tests/unstructured/dg_tests.cpp
@@ -2,6 +2,7 @@
 #include "common.h"
 #include "wireframe.h"
 #include "mesh.h"
+#include "master.h"
 
 int main(int argc, char* argv[])
 {
@@ -25,6 +26,24 @@ int main(int argc, char* argv[])
     }
     std::cout << "Volume = " << vol << std::endl;
     std::cout << "Surface area = " << area << std::endl;
+
+    double qv=0, qa=0;
+    for (const double& w : DG::Quadrature<N,P>::weights) qv += w;
+    for (const double& w : DG::Quadrature<N-1,P>::weights) qa += w;
+
+    vol=0, area=0;
+    for (int i=0; i<mesh.ne; ++i) {
+        double J = mesh.elements[i].simplex.volume();
+        vol += J * qv;
+    }
+    for (int i=0; i<mesh.nf; ++i) {
+        if (mesh.faces[i].boundaryQ()) {
+            double J = mesh.faces[i].simplex.volume();
+            area += J * qa;
+        }
+    }
+    std::cout << "Volume (q) = " << vol << std::endl;
+    std::cout << "Surface area (q) = " << area << std::endl;
 
     return 0;
 }
