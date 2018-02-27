bfdadd7d9a92619a9cd6c0fe8b229bf241a248b3
danfortunato <dan.fortunato@gmail.com>
2018-02-27 15:52:01 -0500

    Test LDG on unstructured meshes

--- tests/unstructured/dg_tests.cpp
+++ tests/unstructured/dg_tests.cpp
@@ -1,50 +1,51 @@
 #include <iostream>
+#include <cmath>
 #include "common.h"
 #include "wireframe.h"
 #include "mesh.h"
 #include "master.h"
+#include "ldgpoisson.h"
 
 int main(int argc, char* argv[])
 {
-    const int N = 3;   // Dimension
-    const int p = 3;   // Polynomial order
+    const int N = 2;   // Dimension
+    const int p = 2;   // Polynomial order
     const int P = p+1; // Number of nodes per dimension
 
-    DG::Wireframe<N> wireframe("data/ball.mesh");
+    DG::Wireframe<N> wireframe("data/circle.mesh");
     std::cout << "Points = " << wireframe.np << ", Triangles = " << wireframe.nt << std::endl;
 
-    auto bnd1 = [] (DG::Tuple<double,N> p) { return std::sqrt(p.square().sum())>1-1e-3; };
-    DG::Mesh<N,P> mesh(wireframe, {bnd1});
+    auto bnd_circle = [] (DG::Tuple<double,N> p) { return std::sqrt(p.square().sum())>1-1e-3; };
+    // auto bnd_square = [] (DG::Tuple<double,N> p) { return (p<1e-3 || p>1-1e-3).any(); };
+    DG::Mesh<N,P> mesh(wireframe, {bnd_circle});
 
-    double vol=0, area=0;
-    for (int i=0; i<mesh.ne; ++i) {
-        vol += mesh.elements[i].volume();
-    }
-    for (int i=0; i<mesh.nf; ++i) {
-        if (mesh.faces[i].boundaryQ()) {
-            area += mesh.faces[i].area();
-        }
-    }
-    std::cout << "Volume = " << vol << std::endl;
-    std::cout << "Surface area = " << area << std::endl;
+    // Circle tests
 
-    double qv=0, qa=0;
-    for (const double& w : DG::Quadrature<N,P>::weights) qv += w;
-    for (const double& w : DG::Quadrature<N-1,P>::weights) qa += w;
+    // Zero Dirichlet
+    auto ufun = [](DG::Tuple<double,N> x) { return (1.0-x.square().sum())/(2.0*N); };
+    auto ffun = [](DG::Tuple<double,N> x) { return 1.0; };
+    DG::BoundaryConditions<N,P> bcs = DG::BoundaryConditions<N,P>::Dirichlet(mesh);
 
-    vol=0, area=0;
-    for (int i=0; i<mesh.ne; ++i) {
-        double J = mesh.elements[i].simplex.volume();
-        vol += J * qv;
-    }
-    for (int i=0; i<mesh.nf; ++i) {
-        if (mesh.faces[i].boundaryQ()) {
-            double J = mesh.faces[i].simplex.volume();
-            area += J * qa;
-        }
-    }
-    std::cout << "Volume (q) = " << vol << std::endl;
-    std::cout << "Surface area (q) = " << area << std::endl;
+    // Nonzero Dirichlet
+    // auto ufun = [](DG::Tuple<double,N> x) { return cos(2*M_PI*x[0])*cos(2*M_PI*x[1]) + x[0] + x[1]; };
+    // auto ffun = [](DG::Tuple<double,N> x) { return 8*M_PI*M_PI*cos(2*M_PI*x[0])*cos(2*M_PI*x[1]); };
+    // DG::BoundaryConditions<N,P> bcs = DG::BoundaryConditions<N,P>::Dirichlet(mesh, ufun);
+
+    // Square tests
+
+    // Zero Dirichlet
+    // auto ufun = [](DG::Tuple<double,N> x) { return sin(2*M_PI*x[0])*sin(2*M_PI*x[1]); };
+    // auto ffun = [](DG::Tuple<double,N> x) { return 8*M_PI*M_PI*sin(2*M_PI*x[0])*sin(2*M_PI*x[1]); };
+
+    DG::Function<N,P> u(mesh, ufun);
+    DG::Function<N,P> f(mesh, ffun);
+    u.write("data/u.fun");
+
+    double tau0 = 100, tauD = 1000;
+    DG::LDGPoisson<N,P> poisson(mesh, bcs, tau0, tauD);
+    DG::Function<N,P> rhs = poisson.computeRHS(f);
+    rhs.write("data/f_rhs.fun");
+    poisson.dump();
 
     return 0;
 }
